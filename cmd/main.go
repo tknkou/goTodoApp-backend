eb9a0448ffb3361a9fee15eecaff5fe3a1bcb5c1
tknkou <kohei.takane@griffithuni.edu.au>
2025-07-27 14:58:23 +0900

    fix cors to allow fromtend request fetch

--- cmd/main.go
+++ cmd/main.go
@@ -21,13 +21,15 @@ func main() {
     router := gin.Default()
     
     //corsの設定
+    allowOrigins := []string{"http://localhost:5173", "https://react-todo-app-front.onrender.com"}
+
     router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:5173"},
+        AllowOrigins:     allowOrigins,
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
         AllowCredentials: true,
-        MaxAge: 12 * time.Hour,
+        MaxAge:           12 * time.Hour,
     }))
 
     //ミドルウェアの設定（トークン認証）
